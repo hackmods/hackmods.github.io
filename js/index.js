633dff4223789565b209e6d09b5a4d6e30d6f66f
Ryan Morris <saveourmacs@gmail.com>
2017-09-29 20:25:10 -0400

    added anchors

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
+
 $(window).bind('scroll', function () {
-    if ($(window).scrollTop() > ($(window).height() * 1.05)) {
+    if ($(window).scrollTop() > ($(window).height() * 1.01)) {
         $('.navbar').removeClass('hide');
     } else {
         $('.navbar').addClass('hide');
